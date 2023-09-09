00396d2919b0198e433cf082bc1e0a1a9971d155
Johannes Scribante <johannes.scribs@gmail.com>
2023-09-09 19:42:25 +0200

    first unit test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/johannesscr/ci
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// add is a function that adds two integers and returns the result.
+func add(x int, y int) int {
+	return x + y
+}
+
+func main() {
+	fmt.Println("Hello, World!")
+}
